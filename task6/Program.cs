bb1f983189695ef7e1a600f17d6675ecc2a39f49
alehey <alexey.p.svyz@mail.ru>
2022-12-02 02:16:30 +0700

    add task6

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Input number");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
