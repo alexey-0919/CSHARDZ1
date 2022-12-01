024101b2ecb732c7e9e00751c31d5d312f0edbf2
alehey <alexey.p.svyz@mail.ru>
2022-12-02 02:08:09 +0700

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+{
+    int max = a;
+    Console.WriteLine("Максимум " + a);
+}
+if (b > a && b > c)
+    {
+        int max = b;
+        Console.WriteLine("Максимум " + b);
+    }
+if (c > a && c > b)
+    {
+        int max = c;
+        Console.WriteLine("Максимум " + c);
+    }
+
