04672670bd3a0abda7674b5d2b033760ea7f1505
alehey <alexey.p.svyz@mail.ru>
2022-12-01 22:55:07 +0700

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число");
+int NumberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int NumberTwo = Convert.ToInt32(Console.ReadLine());
+
+if (NumberOne > NumberTwo)
+{
+    int max = NumberOne;
+    int min = NumberTwo;
+    Console.WriteLine ("Максимум " + max);
+    Console.WriteLine ("Минимум " + min );
+}
+else
+{
+    int max = NumberTwo;
+    int min = NumberOne;
+    Console.WriteLine ("Максимум " + max);
+    Console.WriteLine ("Минимум " + min);
+}
