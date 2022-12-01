45a983eb5bc99ba4b207197185f169a2d7da686d
alehey <alexey.p.svyz@mail.ru>
2022-12-02 02:37:11 +0700

    add task8

---
+++ task8/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Input number");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N > 0)
+{
+
+int i = 1;
+
+while (i <= N)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine (i);
+    }
+i++;
+}
+}
+else
+{
+    Console.WriteLine("Введите целое положительное число");
+}
